2664e911298b3a11249a845f79df1598e53d65cf
kazukinumazato <numazato-kazuki307@g.ecc.u-tokyo.ac.jp>
2023-04-14 00:45:11 +0900

    環境構築修正

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,21 @@ import cors from "cors";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+const { WEB_ORIGIN } = process.env;
 
-app.use(cors({ origin: ["http://localhost:8000"] }));
+app.use(cors({ origin: [WEB_ORIGIN || "http://localhost:8000"] }));
 
 app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:8000"],
+    origin: [WEB_ORIGIN || "http://localhost:8000"],
   },
 });
 
@@ -25,7 +29,7 @@ app.get("/", (request, response) => {
   response.send("connection");
 });
 
-server.listen(3000, () => {
+server.listen(8002, () => {
   // eslint-disable-next-line no-console
   console.log("listening");
 });
